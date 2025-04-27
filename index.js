a22dd5f502ae3e53aea1380f1f495547a248f875
Moaz-Elbeshbeshy <awladnasem800@gmail.com>
2025-04-27 15:37:36 +0200

    finished setting up API endpoint to return the current timestamp in unix and utc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,37 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+// API endpoint to return the current timestamp
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+  let date;
+
+  // check if dateParam is not provided
+  if (!dateParam) {
+    date = new Date();
+  } else {
+    if (isNaN(dateParam)) {
+      // check if dateParam is not a number, which means it is a string utc
+      date = new Date(dateParam);
+    } else {
+      // check if dateParam is a number, which means it is a unix timestamp
+      date = new Date(parseInt(dateParam));
+    }
+  }
+  // check if date is valid
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  } else {
+    // return the date in the required format
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
+})
 
 
 
